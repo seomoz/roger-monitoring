bafa3bb2eb30bfdf941f0893058c95b9ffb981dc
Amit Bose <amit@moz.com>
2016-08-18 22:28:58 -0700

    Removed dryrun of commands

--- ansible/roles/monitoring-influxdb/files/setupinfluxdb.sh
+++ ansible/roles/monitoring-influxdb/files/setupinfluxdb.sh
@@ -24,16 +24,16 @@ echo "*** Setting up Influxdb on your Single node local cluster at $TIME_NOW ***
 
 echo "Installing influxdb..."
 
-#ansible-playbook -i $REPO_DIR/../../roger-mesos/vagrant/single_node/hosts/single $REPO_DIR/monitoring-backend.yml -e @$HOME/rogeros-ansible-secrets.yml.encrypted --vault-password-file ~/.rogeros-ansible-vault-pass --diff --skip-tags="influx_backup,create_influx_databases,retention_policy,influx_users,statsd-fe-influxdb,statsd-repeater" --user=vagrant --ask-pass --check
+ansible-playbook -i $REPO_DIR/../../roger-mesos/vagrant/single_node/hosts/single $REPO_DIR/monitoring-backend.yml -e @$HOME/rogeros-ansible-secrets.yml.encrypted --vault-password-file ~/.rogeros-ansible-vault-pass --diff --skip-tags="influx_backup,create_influx_databases,retention_policy,influx_users,statsd-fe-influxdb,statsd-repeater" --user=vagrant --ask-pass
 
 echo "Creating influxdb users and granting privileges"
 
-#ansible-playbook -i $REPO_DIR/../../roger-mesos/vagrant/single_node/hosts/single $REPO_DIR/monitoring-backend.yml -e @$HOME/rogeros-ansible-secrets.yml.encrypted --vault-password-file ~/.rogeros-ansible-vault-pass --diff --tags="influx_users" --user=vagrant --ask-pass -e "create_db_users_grant_permissions=true" -e "{\"statsd_databases\": $databases }" --check
+ansible-playbook -i $REPO_DIR/../../roger-mesos/vagrant/single_node/hosts/single $REPO_DIR/monitoring-backend.yml -e @$HOME/rogeros-ansible-secrets.yml.encrypted --vault-password-file ~/.rogeros-ansible-vault-pass --diff --tags="influx_users" --user=vagrant --ask-pass -e "create_db_users_grant_permissions=true" -e "{\"statsd_databases\": $databases }"
 
 echo "Creating databases"
 
-#ansible-playbook -i $REPO_DIR/../../roger-mesos/vagrant/single_node/hosts/single $REPO_DIR/monitoring-backend.yml -e @$HOME/rogeros-ansible-secrets.yml.encrypted --vault-password-file ~/.rogeros-ansible-vault-pass --diff --tags="create_influx_databases" --user=vagrant --ask-pass -e "{\"databases_to_backup\": \"$DATABASES\"}"
+ansible-playbook -i $REPO_DIR/../../roger-mesos/vagrant/single_node/hosts/single $REPO_DIR/monitoring-backend.yml -e @$HOME/rogeros-ansible-secrets.yml.encrypted --vault-password-file ~/.rogeros-ansible-vault-pass --diff --tags="create_influx_databases" --user=vagrant --ask-pass -e "{\"databases_to_backup\": \"$DATABASES\"}"
 
 echo "Applying Retention policies on databases"
 
-ansible-playbook -i $REPO_DIR/../../roger-mesos/vagrant/single_node/hosts/single $REPO_DIR/monitoring-backend.yml -e @$HOME/rogeros-ansible-secrets.yml.encrypted --vault-password-file ~/.rogeros-ansible-vault-pass --diff --tags="retention_policy" --user=vagrant --ask-pass -e "apply_retentions_and_cqs=true" -e "{\"databases\": $databases }" -e "tools_database=\"test_db\"" --check
+ansible-playbook -i $REPO_DIR/../../roger-mesos/vagrant/single_node/hosts/single $REPO_DIR/monitoring-backend.yml -e @$HOME/rogeros-ansible-secrets.yml.encrypted --vault-password-file ~/.rogeros-ansible-vault-pass --diff --tags="retention_policy" --user=vagrant --ask-pass -e "apply_retentions_and_cqs=true" -e "{\"databases\": $databases }" -e "tools_database=\"test_db\""
