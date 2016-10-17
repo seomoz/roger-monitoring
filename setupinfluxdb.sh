106a93fd41f7d4d8e69eeabc9460c8266db3d3f6
Amit Bose <amit@moz.com>
2016-10-17 09:19:12 +0530

    Updated Influxdb setup script

--- setupinfluxdb.sh
+++ setupinfluxdb.sh
@@ -24,20 +24,20 @@ echo "*** Setting up Influxdb on your Single node local cluster at $TIME_NOW ***
 
 echo "Installing influxdb..."
 
-ansible-playbook -i $REPO_DIR/../roger-mesos/vagrant/single_node/hosts/single $REPO_DIR/ansible/monitoring-backend.yml -e "@$REPO_DIR/ansible_vars.yml" --diff --skip-tags="influx_backup,create_influx_databases,retention_policy,influx_users,statsd-fe-influxdb,statsd-repeater" --user=vagrant --ask-pass --check
+ansible-playbook -i $REPO_DIR/../roger-mesos/vagrant/single_node/hosts/single $REPO_DIR/ansible/monitoring-backend.yml -e "@$REPO_DIR/ansible_vars.yml" --diff --skip-tags="influx_backup,create_influx_databases,retention_policy,influx_users,statsd-fe-influxdb,statsd-repeater" --user=vagrant --ask-pass
 
 echo "Creating influxdb users and granting privileges"
 
-ansible-playbook -i $REPO_DIR/../roger-mesos/vagrant/single_node/hosts/single $REPO_DIR/ansible/monitoring-backend.yml -e "@$REPO_DIR/ansible_vars.yml" --diff --tags="influx_users" --user=vagrant --ask-pass -e "create_db_users_grant_permissions=true" -e "{\"statsd_databases\": $databases }" --check
+ansible-playbook -i $REPO_DIR/../roger-mesos/vagrant/single_node/hosts/single $REPO_DIR/ansible/monitoring-backend.yml -e "@$REPO_DIR/ansible_vars.yml" --diff --tags="influx_users" --user=vagrant --ask-pass -e "create_db_users_grant_permissions=true" -e "{\"statsd_databases\": $databases }"
 
 echo "Creating databases"
 
-ansible-playbook -i $REPO_DIR/../roger-mesos/vagrant/single_node/hosts/single $REPO_DIR/ansible/monitoring-backend.yml -e "@$REPO_DIR/ansible_vars.yml" --diff --tags="create_influx_databases" --user=vagrant --ask-pass -e "{\"databases_to_backup\": \"$DATABASES {{ tools_database }}\"}" --check
+ansible-playbook -i $REPO_DIR/../roger-mesos/vagrant/single_node/hosts/single $REPO_DIR/ansible/monitoring-backend.yml -e "@$REPO_DIR/ansible_vars.yml" --diff --tags="create_influx_databases" --user=vagrant --ask-pass -e "{\"databases_to_backup\": \"$DATABASES {{ tools_database }}\"}"
 
 echo "Applying Retention policies on databases"
 
-ansible-playbook -i $REPO_DIR/../roger-mesos/vagrant/single_node/hosts/single $REPO_DIR/ansible/monitoring-backend.yml -e "@$REPO_DIR/ansible_vars.yml" --diff --tags="retention_policy" --user=vagrant --ask-pass -e "apply_retentions_and_cqs=true" -e "{\"databases\": $databases }" -e "tools_database=\"{{ tools_database }}\"" --check
+ansible-playbook -i $REPO_DIR/../roger-mesos/vagrant/single_node/hosts/single $REPO_DIR/ansible/monitoring-backend.yml -e "@$REPO_DIR/ansible_vars.yml" --diff --tags="retention_policy" --user=vagrant --ask-pass -e "apply_retentions_and_cqs=true" -e "{\"databases\": $databases }" -e "tools_database=\"{{ tools_database }}\""
 
 echo "Installing and setting up Telegraf for Statsd metrics"
 
-ansible-playbook -i $REPO_DIR/../roger-mesos/vagrant/single_node/hosts/single $REPO_DIR/ansible/monitoring-backend.yml -e "@$REPO_DIR/ansible_vars.yml" --diff --tags="statsd-fe-influxdb" --user=vagrant --ask-pass --check
+ansible-playbook -i $REPO_DIR/../roger-mesos/vagrant/single_node/hosts/single $REPO_DIR/ansible/monitoring-backend.yml -e "@$REPO_DIR/ansible_vars.yml" --diff --tags="statsd-fe-influxdb" --user=vagrant --ask-pass
